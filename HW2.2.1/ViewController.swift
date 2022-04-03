c0aefac623391d283c16f3ad9ff30e3f413b61f5
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2022-04-03 16:29:48 +0500

    ViewController prepared for coding

--- HW2.2.1/ViewController.swift
+++ HW2.2.1/ViewController.swift
@@ -1,11 +1,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colourfulView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var redNumberLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var greenNumberLabel: UILabel!
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var blueNumberLabel: UILabel!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func redSliderMoved() {
+        
+    }
+    
+    @IBAction func greenSliderMoved() {
+        
+    }
+    
+    @IBAction func blueSliderMoved() {
+        
+    }
 }
 
