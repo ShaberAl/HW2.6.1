b4ca488e07b4e6884907a5abc31f0bdd854a377b
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2022-04-03 12:55:43 +0500

    Initial commit

--- HW2.2.1/ViewController.swift
+++ HW2.2.1/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  HW2.2.1
-//
-//  Created by MacOS on 03.04.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,7 +6,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
