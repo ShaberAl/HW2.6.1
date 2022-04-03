0265ffb919247173e7faebba54ef5c7f95f7950c
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2022-04-03 17:59:10 +0500

    ViewController logic coded

--- HW2.2.1/ViewController.swift
+++ HW2.2.1/ViewController.swift
@@ -17,18 +17,38 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        colourfulView.layer.cornerRadius = 20
+        colourfulView.backgroundColor = .black
+        
+        redSlider.value = 0.0
+        greenSlider.value = 0.0
+        blueSlider.value = 0.0
+        
+        redSlider.minimumTrackTintColor = .red
+        redSlider.thumbTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        greenSlider.thumbTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        blueSlider.thumbTintColor = .blue
     }
 
     @IBAction func redSliderMoved() {
-        
+        redNumberLabel.text = changeColour(redValue: CGFloat(redSlider.value), greenValue: CGFloat(greenSlider.value), blueValue: CGFloat(blueSlider.value), sliderValue: redSlider)
     }
     
     @IBAction func greenSliderMoved() {
-        
+        greenNumberLabel.text = changeColour(redValue: CGFloat(redSlider.value), greenValue: CGFloat(greenSlider.value), blueValue: CGFloat(blueSlider.value), sliderValue: greenSlider)
     }
     
     @IBAction func blueSliderMoved() {
+        blueNumberLabel.text = changeColour(redValue: CGFloat(redSlider.value), greenValue: CGFloat(greenSlider.value), blueValue: CGFloat(blueSlider.value), sliderValue: blueSlider)
+    }
+    
+    func changeColour(redValue: CGFloat, greenValue: CGFloat, blueValue: CGFloat, sliderValue: UISlider) -> String {
+        colourfulView.backgroundColor = UIColor(red: CGFloat(redValue), green: CGFloat(greenValue), blue: CGFloat(blueValue), alpha: 1.0)
         
+        return String(round(sliderValue.value * 100) / 100)
     }
 }
 
